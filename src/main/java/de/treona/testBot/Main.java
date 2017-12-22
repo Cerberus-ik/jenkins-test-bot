6ac2036102fff6bcf7872a85ae9e99b3c59edd7a
Cerberus <philipphickisch@gmail.com>
2017-12-22 20:10:21 +0100

    Initial commit

---
+++ src/main/java/de/treona/testBot/Main.java
@@ -0,0 +1,18 @@
+package de.treona.testBot;
+
+import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.exceptions.RateLimitedException;
+
+import javax.security.auth.login.LoginException;
+
+public class Main {
+
+    public static void main(String[] args){
+        try {
+            new JDABuilder(AccountType.BOT).setToken("$TOKEN").buildBlocking();
+        } catch (LoginException | InterruptedException | RateLimitedException e) {
+            e.printStackTrace();
+        }
+    }
+}
